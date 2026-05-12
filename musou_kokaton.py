8117f39675b4cca18045bebdca85ee1565ffff27
C0A25146 <c0a25146e9@edu.teu.ac.jp>
2026-05-12 15:26:27 +0900

    追加機能2実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -242,6 +242,30 @@ class Score:
         screen.blit(self.image, self.rect)
 
 
+class Life:
+    """
+    残機数に関するクラス
+    """
+    def __init__(self, num):
+        """
+        引数 num：初期残機数
+        """
+        self.num = num
+        self.image = pg.Surface((40, 40))
+        self.image.set_colorkey((0, 0, 0))
+        color = (255, 0, 0)
+        points = [(16*math.sin(t/100)**3 +20,
+                   -(13*math.cos(t/100)-5*math.cos(2*t/100)-2*math.cos(3*t/100)-math.cos(4*t/100)) +20
+                   ) for t in range(0,628) ]
+        pg.draw.polygon(self.image, color, points)
+        self.rect = self.image.get_rect()
+        self.rect.center = WIDTH-50, HEIGHT-50
+
+    def update(self, screen):
+        for i in range(self.num):
+            screen.blit(self.image, (WIDTH-120+i*40, HEIGHT-50))
+
+
 def main():
     pg.display.set_caption("真！こうかとん無双")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -253,6 +277,7 @@ def main():
     beams = pg.sprite.Group()
     exps = pg.sprite.Group()
     emys = pg.sprite.Group()
+    life = Life(3)
 
     tmr = 0
     clock = pg.time.Clock()
@@ -283,11 +308,13 @@ def main():
             score.value += 1  # 1点アップ
 
         for bomb in pg.sprite.spritecollide(bird, bombs, True):  # こうかとんと衝突した爆弾リスト
+            life.num -= 1
             bird.change_img(8, screen)  # こうかとん悲しみエフェクト
-            score.update(screen)
-            pg.display.update()
-            time.sleep(2)
-            return
+            if life.num <= 0:
+                score.update(screen)
+                pg.display.update()
+                time.sleep(2)
+                return
 
         bird.update(key_lst, screen)
         beams.update()
@@ -299,6 +326,7 @@ def main():
         exps.update()
         exps.draw(screen)
         score.update(screen)
+        life.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
